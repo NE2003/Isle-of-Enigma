c4979f0ee58ed609e1a2dc3ee323a8d6ae7623e8
NE2003 <86500917+NE2003@users.noreply.github.com>
2024-01-31 14:17:37 +0100

    Basic movement + stamina
    Basic movement + stamina

--- Source/Island_Map/PlayerCharacter.cpp
+++ Source/Island_Map/PlayerCharacter.cpp
@@ -2,33 +2,123 @@
 
 
 #include "PlayerCharacter.h"
-
-// Sets default values
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/CharacterMovementComponent.h"
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    //Calls Tick() every frame.
+    PrimaryActorTick.bCanEverTick = true;
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Player Camera"));
+    Camera->SetupAttachment(RootComponent);
+    Camera->bUsePawnControlRotation = true;
+
+    // Initialize stamina
+    MaxStamina = 100.0f;
+    StaminaDrainRate = 20.0f;
+    StaminaRegenRate = 10.0f;
+    CurrentStamina = MaxStamina;
 
 }
 
-// Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+    CurrentStamina = MaxStamina;
 }
 
-// Called every frame
 void APlayerCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
+    // Drain or regenerate stamina based on running status
+    if (bIsRunning)
+    {
+        CurrentStamina = FMath::Max(0.0f, CurrentStamina - StaminaDrainRate * DeltaTime);
+        if (CurrentStamina <= 0.1 * MaxStamina) // Player becomes exhausted at 10% stamina
+        {
+            bIsRunning = false;
+            UE_LOG(LogTemp, Warning, TEXT("Player Exhausted"));
+        }
+    }
+    else
+    {
+        CurrentStamina = FMath::Min(MaxStamina, CurrentStamina + StaminaRegenRate * DeltaTime);
+    }
+
+    // Display stamina on screen
+    FString StaminaString = FString::Printf(TEXT("Stamina: %.2f"), CurrentStamina);
+    GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Yellow, StaminaString);
 }
 
-// Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+    PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("TurnCamera", this, &APlayerCharacter::Turn);
+    PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::LookUp);
+
+    // Bind functions for running
+    PlayerInputComponent->BindAction("Run", IE_Pressed, this, &APlayerCharacter::StartRunning);
+    PlayerInputComponent->BindAction("Run", IE_Released, this, &APlayerCharacter::StopRunning);
 }
+
+void APlayerCharacter::MoveForward(float InputValue)
+{
+    if (CurrentStamina <= 0)
+    {
+        GetCharacterMovement()->MaxWalkSpeed = 100; // Exhausted
+    }
+    else if (bIsRunning)
+    {
+        GetCharacterMovement()->MaxWalkSpeed = 750; // Running
+    }
+    else
+    {
+        GetCharacterMovement()->MaxWalkSpeed = 350; // Walking
+    }
+
+    FVector ForwardDirection = GetActorForwardVector();
+    AddMovementInput(ForwardDirection, InputValue);
+
+    // Display speed on screen
+    FString SpeedString = FString::Printf(TEXT("Speed: %.2f"), GetCharacterMovement()->Velocity.Size());
+    GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Yellow, SpeedString);
+}
+
+void APlayerCharacter::MoveRight(float InputValue)
+{
+    FVector RightDirection = GetActorRightVector();
+    AddMovementInput(RightDirection, InputValue);
+}
+
+void APlayerCharacter::Turn(float InputValue)
+{
+    AddControllerYawInput(InputValue);
+}
+
+void APlayerCharacter::LookUp(float InputValue)
+{
+    AddControllerPitchInput(InputValue);
+}
+
+void APlayerCharacter::StartRunning()
+{
+    if (CurrentStamina > 60.0f)
+    {
+        bIsRunning = true;
+        UE_LOG(LogTemp, Warning, TEXT("Started Running"));
+    }
+}
+
+void APlayerCharacter::StopRunning()
+{
+    bIsRunning = false;
+    UE_LOG(LogTemp, Warning, TEXT("Stopped Running"));
+}
+
 
--- Source/Island_Map/PlayerCharacter.h
+++ Source/Island_Map/PlayerCharacter.h
@@ -12,18 +12,54 @@ class ISLAND_MAP_API APlayerCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	APlayerCharacter();
+    // Sets default values for this character's properties
+    APlayerCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+
+    UPROPERTY(EditAnywhere)
+    class UCameraComponent* Camera;
+
+    // Stamina properties
+    UPROPERTY(EditAnywhere, Category = "Stamina")
+    float MaxStamina;
+
+    UPROPERTY(EditAnywhere, Category = "Stamina")
+    float CurrentStamina;
+
+    UPROPERTY(EditAnywhere, Category = "Stamina")
+    float StaminaDrainRate;
+
+    UPROPERTY(EditAnywhere, Category = "Stamina")
+    float StaminaRegenRate;
+
+    // Movement speed properties
+    UPROPERTY(EditAnywhere, Category = "Speed")
+    float MaxWalkingSpeed;
+
+    UPROPERTY(EditAnywhere, Category = "Speed")
+    float AdjustedSpeed;
+
+    bool bIsRunning;
+
+    void MoveForward(float InputValue);
+    void MoveRight(float InputValue);
+
+    void Turn(float InputValue);
+    void LookUp(float InputValue);
+
+    // Custom function for handling running with stamina
+    void StartRunning();
+    void StopRunning();
 };
